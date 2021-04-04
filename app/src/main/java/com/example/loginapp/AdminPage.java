378d88cf61770e31de21e7ebaa63c8ca64bfa1da
Russellleung <35394164+Russellleung@users.noreply.github.com>
2021-04-04 15:51:15 +0800

    fixed button

--- app/src/main/java/com/example/loginapp/AdminPage.java
+++ app/src/main/java/com/example/loginapp/AdminPage.java
@@ -18,6 +18,8 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -38,6 +40,9 @@ public class AdminPage extends AppCompatActivity {
     ListView listView;
     FirebaseAuth fAuth=FirebaseAuth.getInstance();
     String newtext;
+    String GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/loginapp-77d35-firebase-adminsdk-v0rgm-25830523b3.json";
+
+
 
 
 
